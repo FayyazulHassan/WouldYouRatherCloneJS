977a08ca65210cd3b622837c72c9126682276dc2
FayyazulHassan <fayyazul@yahoo.com>
2017-04-02 17:42:57 -0500

    Create WouldYouRatherClone.js

---
+++ WouldYouRatherClone.js
@@ -0,0 +1,17 @@
+
+function CreateQuestion() {
+	var x = document.getElementById("Question_Form").getElementsByTagName("input");
+	var field = "";
+	var input = ""
+	var QForm = {};
+  var QFormJson = {};
+	var i;
+	for (i = 0; i < x.length-1 ;i++) {
+		
+		field = "\"" + x[i].name + "\"" 
+		input = "\"" + x[i].value + "\""
+		QForm[field] = input
+		};
+	QFormJson = JSON.stringify(QForm);
+	
+	}
